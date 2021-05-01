99280b7997fea2c8ebdfa5c4130bf6b243eb38e1
Quy Nguyen <hqn650@uowmail.edu.au>
2021-05-01 21:29:00 +1000

    fix interact script

--- interact.sh
+++ interact.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nvidia-docker --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -p 8891:8888 -p 6008:6006 -it --rm --ipc=host -v $PWD:/workspace --name br_gpu br_custom_v1 bash
+nvidia-docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -p 8891:8888 -p 6008:6006 -it --rm --ipc=host -v $PWD:/workspace --name br_gpu br_custom_v1 bash
