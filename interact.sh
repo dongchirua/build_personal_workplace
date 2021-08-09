9145e5ece7627e8a8dcbe446af6cd4499058d0d7
Bryan <dongchirua@users.noreply.github.com>
2021-08-09 10:54:28 +0700

    Update interact.sh

--- interact.sh
+++ interact.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nvidia-docker run --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 -p 8891:8888 -p 6008:6006 -it --rm --ipc=host -v $PWD:/workspace --name br_gpu br_custom_v1 bash
+docker run --gpus all --shm-size=110g --ulimit memlock=-1 --ulimit stack=67108864 -p 8888:8888 -p 6006:6006 -it --rm --ipc=host -v $PWD:/workspace --name br_gpu br_custom_v1 bash
