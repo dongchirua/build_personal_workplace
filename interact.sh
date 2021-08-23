75b9c78ce9d62f993b9731c372d062bfba1cee8b
Bryan <dongchirua@users.noreply.github.com>
2021-08-23 15:51:48 +0700

    update interact run

--- interact.sh
+++ interact.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --gpus all --shm-size=110g --ulimit memlock=-1 --ulimit stack=67108864 -p 8888:8888 -p 6006:6006 -it --rm --ipc=host -v $PWD:/workspace --name br_gpu br_custom_v1 bash
+docker run --gpus all --shm-size=100g --ulimit memlock=-1 --ulimit stack=67108864 -p 8888:8888 -p 6006:6006 -it -d --rm --ipc=host \
+ -v $PWD:/workspace -v /persistent:/persistent --name br_gpu br_custom_v1 jupiter lab
