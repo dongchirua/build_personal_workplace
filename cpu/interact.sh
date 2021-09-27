1df996c00b4bb28f43081e085ff3c46c45e27bfe
Hồng Quy (Bryan) Nguyễn <dongchirua@live.com>
2021-09-28 01:12:02 +0700

    update

--- cpu/interact.sh
+++ cpu/interact.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run --shm-size=100g --ulimit memlock=-1 --ulimit stack=67108864 -p 8888:8888 -it -d --rm --ipc=host \
+docker run --shm-size=100g --ulimit memlock=-1 --ulimit stack=67108864 -p 8888:8888 -it --rm --ipc=host \
  -v $PWD:/workspace --name workspace_cpu workspace:cpu bash
  #jupyter lab --ip=0.0.0.0 --port=8888 --allow-root
